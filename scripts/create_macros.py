25436523a74299688a39560a1fd2079000523d05
Alex Taylor <alex@alext.xyz>
2018-08-29 20:11:43 -0700

    Basic autocomplete with react-select

--- scripts/create_macros.py
+++ scripts/create_macros.py
@@ -1,5 +1,12 @@
+import re
 import csv
 from urllib.parse import quote
+
+
+# DnDBeyond url encoder
+def encode(name):
+    name = name.replace(' ', '-')
+    return re.sub('[^A-Za-z0-9-]+', '', name)
 
 with open('Spells.csv') as infile:
     reader = csv.reader(infile)
@@ -8,14 +15,19 @@ with open('Spells.csv') as infile:
         writer = csv.writer(outfile)
 
         for i, row in enumerate(reader):
+            if not row:
+                continue
+
             if i == 0:
                 writer.writerow(['Macro Name', 'Entity Type', 'Attribute Display Name', 'Value'])
             else:
                 name = row[0]
                 desc = row[8]
-                link = 'https://roll20.net/compendium/dnd5e/{}'.format(quote(name))
+                r20 = 'https://roll20.net/compendium/dnd5e/{}'.format(quote(name))
+                dbeyond = 'https://dndbeyond.com/spells/{}'.format(encode(name))
 
-                writer.writerow([name, 'url', 'Roll20 Spell', link])
+                writer.writerow([name, 'url', 'Roll20 Spell', r20])
+                writer.writerow([name, 'url', 'DnDBeyond Spell', dbeyond])
                 writer.writerow([name, 'text', 'Spell Description', desc])
 
     print('Done!')
--- src/presentation/chatBox.tsx
+++ src/presentation/chatBox.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
+import Select from 'react-select';
+import * as parse from 'csv-parse';
+import 'whatwg-fetch';
 
 import './chatBox.css';
+
+// Async parse function
+function aparse(csv: string): any {
+    return new Promise<any>((resolve, reject) => {
+        parse(csv, (err, res) => {
+            if (err) return reject(err);
+            resolve(res);
+        });
+    });
+}
 
 interface Message {
     author: string;
@@ -9,6 +22,9 @@ interface Message {
 
 interface ChatBoxState {
     messages: Message[];
+    selectValue: any;
+    inputValue: string;
+    options: any[];
 }
 
 interface MessageBoxState {
@@ -59,32 +75,94 @@ export default class ChatBox extends React.Component<any, ChatBoxState> {
     constructor(props: any) {
         super(props);
 
-        this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.readMacros = this.readMacros.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleInputKeyDown = this.handleInputKeyDown.bind(this);
 
         this.state = {
-            messages: []
+            messages: [],
+            selectValue: '',
+            inputValue: '',
+            options: [],
         };
+
+        // Fetch macros
+        this.readMacros().catch((e) => {
+            console.error('Error reading macros: ' + e);
+        });
     }
 
-    handleKeyPress(e: React.KeyboardEvent<any>) {
+    async readMacros() {
+        // TODO: Temporary URL, will need to fetch from Google Drive
+        const res = await fetch('https://cors-anywhere.herokuapp.com/https://transfer.sh/UlNFb/macros.csv',
+            { headers: {'X-Requested-With': 'https://github.com/RobRendell/gTove'}
+        });
+
+        const csv = await res.text();
+
+        const macros: string[][] = await aparse(csv);
+
+        const options = macros.slice(1).map((spell, i) => {
+            if (spell[1] !== 'url') return;
+
+            return {
+                value: <a href={spell[3]}>{spell[0]}</a>,
+                label: spell[0],
+            };
+        });
+
+        this.setState({
+            options: options.filter(el => el !== undefined),
+        })
+    }
+
+    handleChange(value: any) {
+        if (!value.value) return;
+
+        const author = 'sub'; // Test name
+        const message: Message = {
+            author,
+            content: value.value,
+        };
+
+        // Add to array and clear input
+        const messages = this.state.messages.concat(message);
+
+        this.setState({
+            messages,
+            selectValue: null,
+            inputValue: '',
+        });
+    }
+
+    handleInputChange(value: string): string {
+        this.setState({
+            inputValue: value,
+        });
+
+        return value;
+    }
+
+    handleInputKeyDown(e: React.KeyboardEvent<any>) {
         // Send chat
         if (e.key === 'Enter') {
+            e.preventDefault();
+
             const author = 'sub'; // Test name
-            const text: string = (e.target as any).value;
             const message: Message = {
                 author,
-                content: text,
+                content: this.state.inputValue,
             };
 
-            // Add to array
+            // Add to array and clear input
             const messages = this.state.messages.concat(message);
 
             this.setState({
                 messages,
+                selectValue: null,
+                inputValue: '',
             });
-
-            // Clear input
-            (e.target as any).value = '';
         }
     }
 
@@ -92,7 +170,17 @@ export default class ChatBox extends React.Component<any, ChatBoxState> {
         return (
             <div className='chatBox'>
                 <MessageBox {...this.state} />
-                <input type='text' name='chatInput' onKeyPress={this.handleKeyPress} />
+                <Select
+                    options={this.state.options}
+                    value={this.state.selectValue}
+                    className='chatSelect'
+                    arrowRenderer={null}
+                    onChange={this.handleChange}
+                    onInputKeyDown={this.handleInputKeyDown}
+                    onInputChange={this.handleInputChange}
+                    onBlurResetsInput={false}
+                    onCloseResetsInput={false}
+                />
             </div>
         );
     }
